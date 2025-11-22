e364572c06188a97fe89b1c3e9545c1f7512be90
Vitalik <78750613+Vitalik-bog@users.noreply.github.com>
2025-11-22 23:51:15 +0300

    Create user.go

---
+++ project pstgr/models/user.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Age       int       `json:"age"`
+	CreatedAt time.Time `json:"created_at"`
+}
